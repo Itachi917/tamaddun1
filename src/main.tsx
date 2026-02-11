280bc6b5ad8a2bcb24b9f0d1e43d4943d88971e9
Itachi917 <87574052+Itachi917@users.noreply.github.com>
2026-02-12 00:38:54 +0400

    Remove ThemeProvider from main.tsx
    Removed ThemeProvider wrapping around App component.

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-  <App />
-</ThemeProvider>
-
 createRoot(document.getElementById("root")!).render(<App />);
