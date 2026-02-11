a079065bcb0234a6973f71e5225ee887b43318c9
Itachi917 <87574052+Itachi917@users.noreply.github.com>
2026-02-12 00:36:57 +0400

    Wrap App component with ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+  <App />
+</ThemeProvider>
+
 createRoot(document.getElementById("root")!).render(<App />);
