117904ecc75946ceccbb2f65dbac72f10294e38d
Itachi917 <87574052+Itachi917@users.noreply.github.com>
2026-02-12 01:46:40 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ThemeProvider } from "@/components/theme-provider" // <--- ADD THIS
+import { ThemeProvider } from "@/components/theme-provider" 
+import { LanguageProvider } from "@/components/language-provider"
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <App />
+  </ThemeProvider>
+);
+
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider defaultTheme="dark">
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </ThemeProvider>
 );
